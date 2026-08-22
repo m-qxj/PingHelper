81f71570d087c10f6c097b1d4babdc8e0923c350
m-qxj <yyytttmmm626@gmail.com>
2026-08-22 04:40:32 +0000

    Bypass Compose Kotlin version check and rebuild all source files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,10 @@ android {
 
     kotlinOptions {
         jvmTarget = "17"
+        freeCompilerArgs += listOf(
+            "-P",
+            "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true"
+        )
     }
 
     buildFeatures {
