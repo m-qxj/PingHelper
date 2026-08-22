7a372ac254cb48548a94d54fcafc14a5cdaeb564
m-qxj <yyytttmmm626@gmail.com>
2026-08-22 04:20:36 +0000

    Apply full Kotlin 1.9.20 and Compose 1.5.4 compatibility fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -39,6 +38,10 @@ android {
 
     buildFeatures {
         compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.4"
     }
 }
 
